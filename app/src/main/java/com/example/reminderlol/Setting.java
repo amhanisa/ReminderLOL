a3db90415697390fff5761927828015dd78a20bf
Azka Muhammad <azka@amhanisa.com>
2019-11-27 12:20:32 +0700

    yayayaya

--- app/src/main/java/com/example/reminderlol/Setting.java
+++ app/src/main/java/com/example/reminderlol/Setting.java
@@ -2,25 +2,76 @@ package com.example.reminderlol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 
-import java.util.List;
+import androidx.preference.PreferenceFragmentCompat;
 
-public class Setting extends PreferenceActivity {
+public class Setting extends AppCompatActivity {
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferenceFragment()).commit();
+        setContentView(R.layout.setting);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.setting_frag, new PreferenceFragment()).commit();
     }
 
-    public static class PreferenceFragment extends android.preference.PreferenceFragment{
+
+//    @Override
+//    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+//        System.out.println("ada yang berubah");
+//        if(s.equals("dark_preference")){
+//            if(sharedPreferences.getBoolean(s, false)){
+//                findViewById(R.id.setting_frag).setBackgroundColor(Color.parseColor("#333333"));
+//                System.out.println("hey");
+//            } else{
+//                findViewById(R.id.setting_frag).setBackgroundColor(Color.parseColor("#FFFFFF"));
+//                System.out.println("hay");
+//            }
+//        }
+//    }
+
+    public static class PreferenceFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+            System.out.println("ada yang berubah");
+            if (s.equals("dark_preference")) {
+                if (sharedPreferences.getBoolean(s, false)) {
+                    getActivity().findViewById(R.id.setting_frag).setBackgroundColor(Color.parseColor("#333333"));
+                    System.out.println("hey");
+                } else {
+                    getActivity().findViewById(R.id.setting_frag).setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    System.out.println("hay");
+                }
+            }
         }
     }
 }
